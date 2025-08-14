aaa627f59e020378f5199ce257dfc2d1e175ac63
volochayev <stingray8888831@gmail.com>
2025-08-15 01:04:29 +0300

    correct mistakes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useQuery, keepPreviousData } from '@tanstack/react-query'
 import css from "./App.module.css"
 import NoteList from '../NoteList/NoteList'
